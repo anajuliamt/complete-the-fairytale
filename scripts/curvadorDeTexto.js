93f442945234ecf48452b7eb40cc35b341e713b4
Ana Júlia <104026201+anajuliamt@users.noreply.github.com>
2023-06-24 09:51:52 -0300

    Delete curvadorDeTexto.js

--- scripts/curvadorDeTexto.js
+++
@@ -1,7 +0,0 @@
-const textosCurvados = document.querySelectorAll("#texto-curvado");
-
-textosCurvados.forEach((texto) => {
-  const circleType = new CircleType(texto);
-  circleType.dir(1);
-  circleType.radius(800);
-});
